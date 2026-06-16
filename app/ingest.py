ae0e89e54b8f6edac58359c3b66f410c62437202
Shreyansh Vaishnav <vaishnavshreyansh322001@gamil.com>
2026-06-16 21:37:44 +0530

    chunk_size and overlapping changed

--- app/ingest.py
+++ app/ingest.py
@@ -9,7 +9,7 @@ def embed_text(text: str):
 
 from langchain_text_splitters import RecursiveCharacterTextSplitter
 
-def chunk_text(text, chunk_size=1000, chunk_overlap=200):
+def chunk_text(text, chunk_size=400, chunk_overlap=50):
     splitter = RecursiveCharacterTextSplitter(
         chunk_size=chunk_size,
         chunk_overlap=chunk_overlap
